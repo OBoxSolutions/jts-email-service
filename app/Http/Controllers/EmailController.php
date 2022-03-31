455e8fda23ab7285c54264d351c8273c03d015d0
Elodin <jcassola@uclv.cu>
2022-03-30 21:33:01 -0400

    security by token

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,38 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\SendEmailJob;
-use App\Mail\SendEmailClient;
-use App\Mail\SendEmailOwner;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class EmailController extends Controller
 {
-    public function sendEmail(Request $request){
+    public function sendEmail(Request $request)
+    {
 
         $data = $request->all();
 
-        $validator = Validator::make($data, [
-            'address' => 'required|email',
-            'subject' => 'required',
-            'body' => 'required'
-        ]);
+        $hash = hash('sha256', env('ACCESS_TOKEN'));
 
-        if($validator->fails()){
-            return response()->json(['error' => $validator->errors(),
-                                'message'=> 'Incorrect data']);
+        if(($data['token'] === $hash)){
+
+            $validator = Validator::make($data, [
+                'address' => 'required|email',
+                'subject' => 'required',
+                'body' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'error' => $validator->errors(),
+                    'message' => 'Incorrect data'
+                ]);
+            }
+
+            $data['owner'] = env('MAIL_FROM_ADDRESS');
+
+
+
+            SendEmailJob::dispatch($data)->delay(Carbon::now()->addSecond(1));
+            // Mail::to($data['address'])->send(new SendEmailClient($data));
+            // Mail::to(env('MAIL_FROM_ADDRESS'))->send(new SendEmailOwner($data));
+
+            return response()->json(['success', 'Emails sended']);
         }
-
-        $data['owner'] = env('MAIL_FROM_ADDRESS');
-
-
-
-        SendEmailJob::dispatch($data)->delay(Carbon::now()->addSecond(1));
-        // Mail::to($data['address'])->send(new SendEmailClient($data));
-        // Mail::to(env('MAIL_FROM_ADDRESS'))->send(new SendEmailOwner($data));
-
-        return response()->json(['success', 'Emails sended']);
+        else{
+            return response()->json(['failure', 'Incorrect Token! Permission Denied!']);
+        }
     }
 }
